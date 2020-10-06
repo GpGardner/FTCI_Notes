35bd7a09a61093f3e4ef1297c815b6827018bed2
George Gardner <gpgardner@yahoo.com>
2020-10-06 12:54:57 -0400

    notes from bootstrap day

---
+++ javascriptNotes/testingFile.js
@@ -0,0 +1,15 @@
+var temp = 80;
+
+
+if(indoors){
+	console.log("Time to swim");
+}else if(temp>80 && !precipitation){
+	console.log("Time to swim");
+} else {
+	console.log('sorry bro')
+}
+
+if(temp>80 && !precipitation){
+	console.log("Time to swim");
+} 
+
