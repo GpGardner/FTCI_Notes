5454223a4513ece69e45082fca2c2f96a7c28571
George Gardner <gpgardner@yahoo.com>
2020-10-08 08:35:28 -0400

    Changed names of folders

---
+++ javaIntro/StringComparison.java
@@ -0,0 +1,41 @@
+public class StringComparison {
+	public static void main(String[] args) {
+		
+		String word1 = "hello";
+		String word2 = "world";
+		String word3 = "Hello";
+		String word4 = word1;
+		int x = 2;
+
+		//post- increment
+		System.out.println(x++);
+		System.out.println(x--);
+
+		System.out.println(x);
+
+		//pre-increment
+		System.out.println(++x);
+		System.out.println(--x);
+
+		if(true){
+
+
+			if(false){
+				System.out.println("both were true");
+			}
+
+
+			
+		}
+
+
+		
+		
+
+		// System.out.println(byte);
+		// System.out.println(word1.equals(word2));
+		// System.out.println(word1.equals(word3));
+		// System.out.println(word1.equals(word4));
+
+	}
+}
---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+var elem2 = document.querySelector("#headingTwo")
+
+var elem = document.querySelector("#headingOne")
+
+ elem.innerHTML = "this has changed"
+
+ elem2.innerHTML = "this used to say hi there"
+
+
+
+
+
