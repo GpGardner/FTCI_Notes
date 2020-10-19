1d79e6628d8ca6e3feb23a63408b7cd104571917
George Gardner <gpgardner@yahoo.com>
2020-10-19 11:23:12 -0400

    created our subscriber repository

---
+++ subscriberlist/src/main/java/com/tts/subscriberlist/subscriber/SubscriberRepository.java
@@ -0,0 +1,7 @@
+package com.tts.subscriberlist.subscriber;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
+
+}
