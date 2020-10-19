baabde505ec460f622d84d407cd285aed57c341e
George Gardner <gpgardner@yahoo.com>
2020-10-19 11:21:56 -0400

    init commit

---
+++ subscriberlist/src/main/java/com/tts/subscriberlist/SubscriberlistApplication.java
@@ -0,0 +1,13 @@
+package com.tts.subscriberlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberlistApplication.class, args);
+	}
+
+}
