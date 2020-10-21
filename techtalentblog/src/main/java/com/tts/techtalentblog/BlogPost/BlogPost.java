7a576fb669fbc7f42fdcd48e36e738cf0cdb56f0
George Gardner <gpgardner@yahoo.com>
2020-10-21 09:35:58 -0400

    Included annotations to work with JPA

--- techtalentblog/src/main/java/com/tts/techtalentblog/BlogPost/BlogPost.java
+++ techtalentblog/src/main/java/com/tts/techtalentblog/BlogPost/BlogPost.java
@@ -1,8 +1,17 @@
 package com.tts.techtalentblog.BlogPost;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class BlogPost {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
 	private String title;
 	private String author;
 	private String blogEntry;
