341b44027803b82bb2de30dc47c5bc070c567ead
George Gardner <gpgardner@yahoo.com>
2020-10-21 11:06:12 -0400

    Added a list to contain blogPosts

--- techtalentblog/src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
+++ techtalentblog/src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
@@ -1,5 +1,8 @@
 package com.tts.techtalentblog.BlogPost;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +15,20 @@ public class BlogPostController {
 	@Autowired
 	private BlogPostRepository blogPostRepository;
 
+	private List<BlogPost> posts = new ArrayList<>();
+
 	//localhost:8080
 	//Handle get request to forward slash
 	@GetMapping(value = "/")
-	public String index(BlogPost blogPost) {
+	public String index(BlogPost blogPost, Model model) {
+		model.addAttribute("posts", posts);
 		return "blogpost/index";
 	}
 
 	@PostMapping(value = "/")
 	public String addNewBlogPost(BlogPost blogPost, Model model) {
 		blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
+		posts.add(blogPost);
 		model.addAttribute("title", blogPost.getTitle());
 		model.addAttribute("author", blogPost.getAuthor());
 		model.addAttribute("blogEntry", blogPost.getBlogEntry());
