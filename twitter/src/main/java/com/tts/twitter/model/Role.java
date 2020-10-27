75a5fde93121d06ce90bc0f3e2fb2c9c94d1a457
George Gardner <gpgardner@yahoo.com>
2020-10-27 09:48:39 -0400

    Completed Role model

---
+++ twitter/src/main/java/com/tts/twitter/model/Role.java
@@ -0,0 +1,48 @@
+package com.tts.twitter.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "role_id")
+	private Long id;
+	private String role;
+	
+	public Role() {}
+
+	public Role(Long id, String role) {
+		this.id = id;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", role=" + role + "]";
+	}
+
+	
+	
+}
