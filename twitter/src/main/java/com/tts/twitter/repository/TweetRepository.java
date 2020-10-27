1cccfdc2446dd5487df0f2f745d2410ffe097c3c
George Gardner <gpgardner@yahoo.com>
2020-10-27 12:49:16 -0400

    Added tweet repo

---
+++ twitter/src/main/java/com/tts/twitter/repository/TweetRepository.java
@@ -0,0 +1,16 @@
+package com.tts.twitter.repository;
+
+import java.util.List;
+
+import com.tts.twitter.model.Tweet;
+import com.tts.twitter.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends CrudRepository<Tweet, Long> {
+	List<Tweet> findAllByOrderByCreatedAtDesc();
+	List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
+	List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
+}
