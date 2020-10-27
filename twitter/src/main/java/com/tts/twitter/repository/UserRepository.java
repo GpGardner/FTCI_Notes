90ec015d68ad5db91ed696bb679e581430a72ee6
George Gardner <gpgardner@yahoo.com>
2020-10-27 12:49:21 -0400

    Added user repo

---
+++ twitter/src/main/java/com/tts/twitter/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.tts.twitter.repository;
+
+import com.tts.twitter.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+	
+	User findByUsername(String username);
+	
+}
