d5898e51bb27145f45f67f56c1c03f607ca2de92
George Gardner <gpgardner@yahoo.com>
2020-10-27 12:47:27 -0400

    Model for user

--- twitter/src/main/java/com/tts/twitter/model/User.java
+++ twitter/src/main/java/com/tts/twitter/model/User.java
@@ -9,9 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.JoinColumn;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -58,84 +58,80 @@ public class User {
 	public Long getId() {
 		return id;
 	}
-
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public String getEmail() {
 		return email;
 	}
-
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
 	public String getUsername() {
 		return username;
 	}
-
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	
 	public String getPassword() {
 		return password;
 	}
-
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
 	public String getFirstName() {
 		return firstName;
 	}
-
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
+	
 	public String getLastName() {
 		return lastName;
 	}
-
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
+	
 	public int getActive() {
 		return active;
 	}
-
+	
 	public void setActive(int active) {
 		this.active = active;
 	}
-
+	
 	public Date getCreatedAt() {
 		return createdAt;
 	}
-
+	
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-
+	
 	public Set<Role> getRoles() {
 		return roles;
 	}
-
+	
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "User [active=" + active + ", createdAt=" + createdAt + ", email=" + email + ", firstName=" + firstName
 				+ ", id=" + id + ", lastName=" + lastName + ", password=" + password + ", roles=" + roles + ", username="
 				+ username + "]";
 	}
-
 	
-
-	
-
 }
