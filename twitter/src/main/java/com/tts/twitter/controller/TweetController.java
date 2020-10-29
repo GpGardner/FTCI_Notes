7e7a961dfbef16e63bea9562e48be1f33e4c06c4
George Gardner <gpgardner@yahoo.com>
2020-10-29 10:01:19 -0400

    Added handling for get mapping to tweets/{tag}

--- twitter/src/main/java/com/tts/twitter/controller/TweetController.java
+++ twitter/src/main/java/com/tts/twitter/controller/TweetController.java
@@ -14,39 +14,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TweetController {
-	
+
 	@Autowired
 	private UserService userService;
 
 	@Autowired
 	private TweetService tweetService;
 
-	@GetMapping(value= {"/tweets", "/"}) //URL
-	public String getFeed(Model model){
-			List<Tweet> tweets = tweetService.findAll();
-			model.addAttribute("tweetList", tweets);
-			return "feed"; //HTML
+	@GetMapping(value = { "/tweets", "/" }) // URL
+	public String getFeed(Model model) {
+		List<Tweet> tweets = tweetService.findAll();
+		model.addAttribute("tweetList", tweets);
+		return "feed"; // HTML
 	}
 
-	@GetMapping(value = "/tweets/new") //URL
+	@GetMapping(value = "/tweets/new") // URL
 	public String getTweetForm(Model model) {
-			model.addAttribute("tweet", new Tweet());
-			return "newTweet";
+		model.addAttribute("tweet", new Tweet());
+		return "newTweet";
 	}
-		
-	@PostMapping(value = "/tweets") //URL
+
+	// http://localhost:8080/tweets/java
+	@GetMapping(value = "/tweets/{tag}")//URL
+																										//java
+	public String getTweetsByTag(@PathVariable(value = "tag") String tag, Model model) {
+		List<Tweet> tweets = tweetService.findAllWithTag(tag);
+		model.addAttribute("tweetList", tweets);
+		model.addAttribute("tag", tag);
+		return "taggedTweets";//HTML
+	}
+
+	@PostMapping(value = "/tweets") // URL
 	public String submitTweetForm(@Valid Tweet tweet, BindingResult bindingResult, Model model) {
 		User user = userService.getLoggedInUser();
 		if (!bindingResult.hasErrors()) {
-				tweet.setUser(user);
-				tweetService.save(tweet);
-				model.addAttribute("successMessage", "Tweet successfully created!");
-				model.addAttribute("tweet", new Tweet());
+			tweet.setUser(user);
+			tweetService.save(tweet);
+			model.addAttribute("successMessage", "Tweet successfully created!");
+			model.addAttribute("tweet", new Tweet());
 		}
-		return "newTweet"; //HTML
+		return "newTweet"; // HTML
 	}
 }
