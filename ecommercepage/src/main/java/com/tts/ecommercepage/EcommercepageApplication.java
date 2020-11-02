199f5fc8304707f0169dee8031bc6738273e0a3f
George Gardner <gpgardner@yahoo.com>
2020-11-02 10:02:33 -0500

    init commit

---
+++ ecommercepage/src/main/java/com/tts/ecommercepage/EcommercepageApplication.java
@@ -0,0 +1,13 @@
+package com.tts.ecommercepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercepageApplication.class, args);
+	}
+
+}
---
+++ ecommercepage/src/test/java/com/tts/ecommercepage/EcommercepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.ecommercepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
