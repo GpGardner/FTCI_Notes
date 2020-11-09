b2295aea626a643d7b2562eb9335102a7d97743d
George Gardner <gpgardner@yahoo.com>
2020-11-09 10:07:34 -0500

    Added home mapping

---
+++ mapsapp/src/main/java/com/tts/mapsapp/controller/MapController.java
@@ -0,0 +1,14 @@
+package com.tts.mapsapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+	
+	@GetMapping("/home")
+	public String getDefaultMap(){
+		return "index";
+	}
+
+}
