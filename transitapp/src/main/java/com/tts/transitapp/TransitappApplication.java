6cd7ba30259bc4ac5eb241c62f66e6ef7a532e19
George Gardner <gpgardner@yahoo.com>
2020-11-10 09:23:17 -0500

    init commit

---
+++ transitapp/src/main/java/com/tts/transitapp/TransitappApplication.java
@@ -0,0 +1,13 @@
+package com.tts.transitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitappApplication.class, args);
+	}
+
+}
---
+++ transitapp/src/test/java/com/tts/transitapp/TransitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.transitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
