c31218c7f098fc6a377d2293a58b47f973bd30d7
George Gardner <gpgardner@yahoo.com>
2020-11-10 14:43:40 -0500

    front - end

---
+++ transitapp/src/main/resources/static/custom.js
@@ -0,0 +1,21 @@
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: parseFloat(busLocations[0].LATITUDE),
+      lng: parseFloat(busLocations[0].LONGITUDE),
+    },
+    zoom: 15,
+    scrollwheel: false,
+  });
+
+  for (i = 0; i < busLocations.length; i++) {
+    let marker = new google.maps.Marker({
+      position: {
+        lat: parseFloat(busLocations[i].LATITUDE),
+        lng: parseFloat(busLocations[i].LONGITUDE),
+      },
+      map: map,
+    });
+  }
+}
