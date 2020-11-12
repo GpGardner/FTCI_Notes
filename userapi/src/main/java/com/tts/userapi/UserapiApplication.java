cbd03fe30b0bc53500f079da22a97116f0e875b6
George Gardner <gpgardner@yahoo.com>
2020-11-12 09:15:06 -0500

    init commit

---
+++ userapi/src/main/java/com/tts/userapi/UserapiApplication.java
@@ -0,0 +1,13 @@
+package com.tts.userapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserapiApplication.class, args);
+	}
+
+}
---
+++ userapi/src/test/java/com/tts/userapi/UserapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
