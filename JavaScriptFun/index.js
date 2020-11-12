ccbdc1f7c0b43684e32a95d934a607d61126fdda
George Gardner <gpgardner@yahoo.com>
2020-11-12 14:22:03 -0500

    better example

--- JavaScriptFun/index.js
+++ JavaScriptFun/index.js
@@ -1,17 +1,26 @@
 const url = "https://gentle-chamber-40438.herokuapp.com";
 
-// axios.get(url + "/users").then(response => console.log(response.data[0]));
-
-function makeGetRequest() {
+const makeGetRequest = () => {
   console.log(url + "/users");
   axios.get(url + "/users").then(
     (response) => {
       let result = response.data;
-      console.log(result);
-      document.getElementById("test").innerHTML = result[0].firstName;
+      makeCards(result);
     },
     (error) => {
       console.log(error);
     }
   );
-}
+};
+
+const makeCards = (result) => {
+  let element = document.getElementById("test");
+  result.forEach((user) => {
+    let card = document.createElement("DIV");
+    let cardHTML = document.createTextNode(
+      user.firstName + " " + user.lastName + ": " + user.state
+    );
+    card.appendChild(cardHTML);
+    element.appendChild(card);
+  });
+};
