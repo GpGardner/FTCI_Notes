b13b7bb33a3dc8758d642da13cc63942567e71dc
George Gardner <gpgardner@yahoo.com>
2020-11-13 20:41:46 -0500

    Added testing data, and functions to handle logic

--- JavaScriptFun/index.js
+++ JavaScriptFun/index.js
@@ -1,5 +1,6 @@
 const url = "https://gentle-chamber-40438.herokuapp.com";
 
+//REQUEST TO API - API WE MADE IN CLASS
 const makeGetRequest = () => {
   console.log(url + "/users");
   axios.get(url + "/users").then(
@@ -13,15 +14,79 @@ const makeGetRequest = () => {
   );
 };
 
-const makeCards = (result) => {
+//TESTING DATA, USED IF WORKING OFFLINE
+const testingCards = () => {
+  let userData = [
+    {
+      firstName: "George",
+      lastName: "Gardner",
+      state: "North Carolina",
+    },
+    {
+      firstName: "Tia",
+      lastName: "Fischl",
+      state: "North Carolina",
+    },
+    {
+      firstName: "Mela",
+      lastName: "Gardner",
+      state: "Pennsylvania",
+    },
+  ];
+  makeCards(userData);
+};
+
+//FUNCTION TO CREATE CARDS FOR WHICHEVER USERS GET PASSED IN
+const makeCards = (users) => {
+  //GET REFERENCE TO ELEMENT IN HTML
   let element = document.getElementById("test");
-  result.forEach((user) => {
-    let card = document.createElement("DIV");
-    let cardHTML = document.createTextNode(
-      user.firstName + " " + user.lastName + ": " + user.state
-    );
-    card.appendChild(cardHTML);
-    card.classList.add("card", "mt-5`");
-    element.appendChild(card);
+  //ARRAY LIST TO HOLD STATES THAT HAVE BEEN MADE ALREADY
+  let listOfStatesAlreadyCreated = [];
+
+  //LOOP OVER USERS FROM ABOVE, EACH USER WILL BE ITERADED OVER
+  users.forEach((user) => {
+    //CHECK THE ARRAYLIST IF IT HAS ALREADY CREATED A CARD FOR A STATE
+    if (listOfStatesAlreadyCreated.includes(user.state)) {
+      addUserToCard(user);
+    } else {
+      //ADD THE CURRENT STATE TO THE LIST, THEN CREATE A CARD
+      listOfStatesAlreadyCreated.push(user.state);
+      createNewCardForState(user, element);
+    }
   });
+};
+
+//LOGIC TO CREATE A WHOLE NEW CARD FOR A STATE THAT HASN'T BEEN CREATED YET
+const createNewCardForState = (user, element) => {
+  let card, cardHeader, cardBody, cardState, cardName;
+  card = document.createElement("DIV");
+  cardHeader = document.createElement("DIV");
+  cardBody = document.createElement("DIV");
+  cardBodyList = document.createElement("LI");
+  cardState = document.createTextNode(user.state);
+  cardName = document.createTextNode(user.firstName + " " + user.lastName);
+
+  cardHeader.appendChild(cardState);
+  cardHeader.classList.add("card-header");
+
+  cardBody.appendChild(cardBodyList);
+  cardBodyList.appendChild(cardName);
+  cardBody.classList.add("card-body");
+  cardBody.id = user.state;
+
+  card.appendChild(cardHeader);
+  card.appendChild(cardBody);
+  card.classList.add("card", "mt-3");
+
+  element.appendChild(card);
+};
+
+//LOGIC FOR ADDING A USER TO AN EXISTING STATE CARD
+const addUserToCard = (user) => {
+  let card, cardName, cardBodyList;
+  card = document.getElementById(user.state);
+  cardBodyList = document.createElement("LI");
+  cardName = document.createTextNode(user.firstName + " " + user.lastName);
+  cardBodyList.appendChild(cardName);
+  card.appendChild(cardBodyList);
 };
