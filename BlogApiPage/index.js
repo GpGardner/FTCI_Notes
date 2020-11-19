2b9aa4629fbf3424d9eb32433dd86a0498be8305
George Gardner <gpgardner@yahoo.com>
2020-11-19 13:05:18 -0500

    Updated

--- BlogApiPage/index.js
+++ BlogApiPage/index.js
@@ -19,13 +19,12 @@ function deleteBlogPost(id) {
   axios.delete(`${url}/${id}`).then(
     (response) => {
       console.log(response);
+      location.reload();
     },
     (error) => {
       console.log(error);
     }
   );
-
-  location.reload();
 }
 
 const makePostRequest = () => {
@@ -46,6 +45,7 @@ const makePostRequest = () => {
     .post(url, blogPost)
     .then(function (response) {
       console.log(response);
+      location.reload();
     })
     .catch(function (error) {
       console.log(error);
@@ -55,12 +55,12 @@ const makePostRequest = () => {
 function createBlogPostView(blogPosts) {
   blogPosts.forEach((blogPost) => {
     let cardBodyElement = document.createElement("div");
-    cardBodyElement.innerHTML = `<div class="card mt-5" style="width: 18rem;">
+    cardBodyElement.innerHTML = `<div class="card mt-5 col-4" style="max-width: 18rem;">
 		<img class="card-img-top" src="${blogPost.image}" alt="Card image cap">
 		<div class="card-body">
 			<h5 class="card-title">${blogPost.title}</h5>
 			<p class="card-text">${blogPost.blogEntry}</p>
-			<button class="btn btn-danager" onclick="deleteBlogPost(${blogPost.id})"/>
+			<div class="btn btn-danger mt-1" onclick="deleteBlogPost(${blogPost.id})">Delete</div>
 		</div>
 	</div>`;
 
