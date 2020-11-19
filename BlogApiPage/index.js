43a8a565b90cdc756f8feba1906c9e125edbe414
George Gardner <gpgardner@yahoo.com>
2020-11-19 12:20:29 -0500

    EASIER VERSION OF HTML TEMPLATING

--- BlogApiPage/index.js
+++ BlogApiPage/index.js
@@ -41,7 +41,7 @@ const makePostRequest = () => {
     blogEntry,
   };
 
-	console.log(blogPost)
+  console.log(blogPost);
   axios
     .post(url, blogPost)
     .then(function (response) {
@@ -54,45 +54,17 @@ const makePostRequest = () => {
 
 function createBlogPostView(blogPosts) {
   blogPosts.forEach((blogPost) => {
-    let imgElement = document.createElement("img");
-    imgElement.src = blogPost.image;
-
-    let title = document.createTextNode(blogPost.title);
-    let titleElement = document.createElement("h5");
-    titleElement.classList.add("card-title");
-    titleElement.appendChild(title);
-
-    let author = document.createTextNode(blogPost.author);
-    let authorElement = document.createElement("div");
-    authorElement.classList.add("card-subtitle");
-    authorElement.classList.add("mb-2");
-    authorElement.classList.add("text-muted");
-    authorElement.appendChild(author);
-
-    let blogEntry = document.createTextNode(blogPost.blogEntry);
-    let blogEntryElement = document.createElement("div");
-    blogEntryElement.classList.add("card-body");
-    blogEntryElement.appendChild(blogEntry);
-
-    let deleteButtonElement = document.createElement("button");
-    deleteButtonElement.innerHTML = `<button class="btn btn-danager" onclick="deleteBlogPost(${blogPost.id})"/>`;
-
-    let cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-
     let cardBodyElement = document.createElement("div");
-    cardBodyElement.classList.add("card-body");
+    cardBodyElement.innerHTML = `<div class="card mt-5" style="width: 18rem;">
+																	<img class="card-img-top" src="${blogPost.image}" alt="Card image cap">
+																	<div class="card-body">
+																		<h5 class="card-title">${blogPost.title}</h5>
+																		<p class="card-text">${blogPost.blogEntry}</p>
+																		<button class="btn btn-danager" onclick="deleteBlogPost(${blogPost.id})"/>
+																	</div>
+																</div>`;
 
-    cardBodyElement.appendChild(titleElement);
-    cardBodyElement.appendChild(authorElement);
-    cardBodyElement.appendChild(blogEntryElement);
-
-    cardElement.appendChild(imgElement);
-    cardElement.appendChild(cardBodyElement);
-    cardBodyElement.appendChild(deleteButtonElement);
-    cardElement.classList.add("mt-5");
-
-    postsDiv.appendChild(cardElement);
+    postsDiv.appendChild(cardBodyElement);
   });
 }
 
